3bea45e7a19839520dc037e86b38d97f6bc472f3
Weblate Admin <blacksheep@nubecula.org>
2021-11-22 18:18:54 +0000

    Added translation using Weblate (Polish)

---
+++ translations/harbour-porthole-pl.ts
@@ -0,0 +1,562 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Give star</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you like my work why not buy me a beer?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donate with PayPal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donate with Liberpay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Porthole</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Porthole is a Sailfish OS application to control a Pi-hole server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AnalysisPage</name>
+    <message>
+        <source>Top permitted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show top permitted domains</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show top blocked domains</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top clients</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show top clients</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Top clients blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show top blocked clients</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CounterListPage</name>
+    <message numerus="yes">
+        <source>%n Request(s)</source>
+        <comment>0</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Total Queries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Queries Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Percent Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditFilterDialog</name>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Domain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>RegEx filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter domain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter regular expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Regular Expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add domain as wildcard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Activate this to involve all subdomains</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter description (optional)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dashboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ad blocking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable / disable ad blocking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Today Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Total Queries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Queries Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Percent Blocked</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Domains on Blocklist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Total Clients</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Analysis</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsConnectionPage</name>
+    <message>
+        <source>Connection Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter URL (e.g. http://pi-hole.local)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Access Token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter access token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsFiltersPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Whitelist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Blacklist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No filters available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to add one</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to delete filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to add filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Infos about Porthole</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show server info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Whitelist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Manage domains on whitelist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Blacklist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Manage domains on blacklist</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsServerInfoPage</name>
+    <message>
+        <source>Server Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Core</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Web</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>FTL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Software Versions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gravity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>no</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Timespan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <source>%n day(s)</source>
+        <comment>0</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n hour(s)</source>
+        <comment>0</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n minute(s)</source>
+        <comment>0</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>WizardConnectionPage</name>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connection Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Valid URL required!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You need to provide the connection details to your Pi-hole server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please provide a full URL for this!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter URL (e.g. http://pi-hole.local)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to connect to Pi-hole server!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WizardFinalPage</name>
+    <message>
+        <source>Complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Setup process completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WizardIntroPage</name>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Welcome to Porthole</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This app is not configured yet.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The setup wizard will guide you through the configuration process.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If there are errors during the setup process, you can try to reset the data from a previous installation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Resetting application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WizardTokenPage</name>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Access Token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter access token</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You need to provide an access token to connect to Pi-hole server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This token can be found in config file e.g. &quot;/etc/pihole/setupVars.conf&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search for entry WEBPASSWORD.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-porthole</name>
+    <message>
+        <source>Connection refused</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Host not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connection timed out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ssl handshake failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Access denied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal server error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unkown connection error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
